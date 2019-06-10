5592c0200157b8c1e9fa33fd7b83284bd2462dd9
ZhangWurui <49122713+ZhangWurui@users.noreply.github.com>
2019-06-10 14:25:03 +0800

    更新login.vue
    1.修改：将存储在缓存中的token过期时间，从expired_ms改为expired_at
    2.删除：测试时写死的数据
    3.修改：全局变量webSiteUrl使用
    4.修改：request请求的成功判断标志改用res.statusCode
    5.修改：request请求返回的json数据的使用，从res修正为res.data
    6.修改：由于时间格式的修改，对用户登陆验证的request返回的json数据中的token过期时间的”格式化“过程简化

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -43,10 +43,9 @@
 				if(res.keys[0] == 'userName'){
 					console.log("存储过该用户的信息");
 					// 检查token是否到期
-					let expired_ms = uni.getStorageSync('expired_ms');
+					let expired_at = new Date(uni.getStorageSync('expired_at'));
 					let current_time = new Date();
-					let current_ms = current_time.getTime();
-					if(expired_ms>current_ms){
+					if(expired_at>current_time){
 						uni.redirectTo({
 							url: '../home/home'
 						});
@@ -91,23 +90,20 @@
                     return;
                 }
                 // 检测用户账号密码是否在已注册的用户列表中
-				// 这里token为了测试，将过期时间设置在2019-06-30
-				var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVkX2F0IjoiVGh1IEp1biAzMCAwMjoyMTo0NiBVVEMgMjAxOSIsImlkIjoxLCJyb2xlIjoibWFzdGVyIn0=.NwUOTTY5ZbPVRQdQRFQwZ3Na-hNfemMrFSTGv63aJjs';
+				var token;
 				uni.request({
-					url: 'https://webSiteUrl/user/auth',
+					url: webSiteUrl+'user/auth',
 					method: 'POST',
 					data: {
 						name:this.account,
 						password:this.password
 					},
 					success: (res) => {
-						console.log(res);
 						token = res.data.token;
-						if (res.status==200) {
+						if (res.statusCode==200) {
 							let userName = this.account;
 							// 提取token中的相关信息
 							let key_info_base64 = token.split(".")[1];
-							console.log("key_info_base64="+key_info_base64);
 							// base64解码的js实现（atob函数用不了，只能强行转）
 							var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";  
 							var key_info_str = "";  
@@ -135,30 +131,15 @@
 							let key_info_obj = JSON.parse(key_info_str);
 							console.log(key_info_obj);
 							let user_id = key_info_obj.id;
-							let expired_at_init = key_info_obj.expired_at;
+							let expired_at = key_info_obj.expired_at;
 							// 将expired_at格式化
-							let expired_at_splited = expired_at_init.split(" ");
-							let month_init = expired_at_splited[1];
-							let day = parseInt(expired_at_splited[2]);
-							let time = expired_at_splited[3].split(":");
-							let hours = parseInt(time[0])-1;
-							let minutes = parseInt(time[1])-1;
-							let seconds = parseInt(time[2])-1;
-							let year = parseInt(expired_at_splited[5]);
-							let month_array = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-							var month;
-							for (var i = 0; i < 12; i++) {
-								if(month_init==month_array[i]){
-									month = i;
-									break;
-								}
-							}
-							let expired_ms = Date.UTC(year,month,day,hours,minutes,seconds);
+							// expired_at_init=new Date(expired_at_init)
+							// let expired_at = Date.UTC(expired_at_init.getYear(),expired_at_init.getMonth(),expired_at_init.getDay(),expired_at_init.getHours(),expired_at_init.getMinutes(),expired_at_init.getSeconds());
 							// 存储用户登陆信息到本地
 							try{
 								uni.setStorageSync('userName',userName);
 								uni.setStorageSync('token',token);
-								uni.setStorageSync('expired_ms',expired_ms);
+								uni.setStorageSync('expired_at',expired_at);
 								uni.setStorageSync('user_id',user_id);
 							}catch(e){
 								console.log("存储出现问题");
@@ -200,6 +181,6 @@
 	
 	.title {
 	    color: #8f8f94;
-		text-align:center;//水平居中
+		text-align:center;
 	}
 </style>
